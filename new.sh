f316d0a326dfe86a99264b1d5d9fb2b27b6fde36
Rentoo <dreamyiz@outlook.com>
2020-07-01 02:12:01 +0800

    new.sh create new blog

---
+++ new.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+new_file_name=_posts/$(date '+%Y-%m-%d-')
+echo $new_file_name
+new_line="date:   $(date '+%Y-%m-%d %H:%M:%S +0800')"
+cp _template/template-1.markdown "${new_file_name}"
+
+sed -i '' '4s/.*/'"$new_line"'/' ${new_file_name}
+# sed -i '4s/.*/'"$new_line"'/' ./${new_file_name} 
+# sed -i '4s/.*/'"$new_line"'/'  "${new_file_name}" < "${template}"
+# sed '2a\
+# text to insert
+# ' < "${new_file_name}"
+# echo "$(date '+%Y-%m-%d %H:%M:%S +0800')" >> "${new_file_name}"
