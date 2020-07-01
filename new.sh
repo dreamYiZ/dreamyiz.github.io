a0e215c5094ba38a57b925ce3006d50f9a55d82f
Rentoo <dreamyiz@outlook.com>
2020-07-01 14:00:44 +0800

    new.sh optimize

--- new.sh
+++ new.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-new_file_name=_posts/$(date '+%Y-%m-%d-')$1
+new_file_name=_posts/$(date '+%Y-%m-%d-')$1.markdown
 echo $new_file_name
+new_line2="title:   $1"
 new_line="date:   $(date '+%Y-%m-%d %H:%M:%S +0800')"
 cp _template/template-1.markdown "${new_file_name}"
 
+sed -i '' '3s/.*/'"$new_line2"'/' ${new_file_name}
 sed -i '' '4s/.*/'"$new_line"'/' ${new_file_name}
+
 # sed -i '4s/.*/'"$new_line"'/' ./${new_file_name} 
 # sed -i '4s/.*/'"$new_line"'/'  "${new_file_name}" < "${template}"
 # sed '2a\
